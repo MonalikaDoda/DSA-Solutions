2437b33271e3362f4a2ea3b7b19f5b3124f85420
Monalika <153423793+MonalikaDoda@users.noreply.github.com>
2026-08-25 12:42:21 +0530

    [33] [Time Beats: 100.00%] [Memory Beats: 96.80%] - LeetPush

---
+++ 33-Search-in-Rotated-Sorted-Array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = start + (end - start)/2;
+        while(start<=end){
+
+            if(nums[mid]==target){
+                return mid;
+            }
+
+            if(nums[mid]>=nums[start]){ //left sorted
+                if(nums[start] <= target && target <= nums[mid]){
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
+            }
+
+            else{
+                if(nums[mid] <= target && target <= nums[end]){
+                    start = mid + 1;
+                }
+                else{
+                    end = mid - 1;
+                }
+            }
+            mid = start + (end - start)/2;
+        }
+        return -1;
+    }
+};
