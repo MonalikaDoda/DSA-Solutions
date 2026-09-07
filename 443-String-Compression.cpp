df15d1b69dbbde7c625a61e2ac82a8ea0fb6af78
Monalika <153423793+MonalikaDoda@users.noreply.github.com>
2026-09-07 12:30:49 +0530

    [443] [Time Beats: 100.00%] [Memory Beats: 16.56%] - LeetPush

---
+++ 443-String-Compression.cpp
@@ -0,0 +1,30 @@
+//Very important question
+
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i = 0;
+        int ansIndex = 0;
+        int n = chars.size();
+
+        while(i<n){
+            int j = i+1;
+            while(j<n && chars[i]==chars[j]){
+                j++;
+            }
+            chars[ansIndex] = chars[i];
+            ansIndex++;
+            int count = j-i;
+            if (count > 1) {
+                string countStr = to_string(count);
+
+                for (char c : countStr) {
+                    chars[ansIndex] = c;
+                    ansIndex++;
+                }
+            }
+            i=j;
+        }
+        return ansIndex;     
+    }
+};
